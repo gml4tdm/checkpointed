fb1559608cd3ff09f1ec39c4d4e0a67fa86707e6
Jesse Maarleveld <jessemaarleveld@gmail.com>
2024-01-10 09:53:11 +0100

    ScatterGather V1 [untested]

--- checkpointed-core/checkpointed_core/subpipeline.py
+++ checkpointed-core/checkpointed_core/subpipeline.py
@@ -1,4 +1,6 @@
 import abc
+import collections
+import itertools
 import os
 import typing
 
@@ -15,41 +17,9 @@ class ScatterGather(abc.ABC, PipelineStep):
 
     async def execute(self, **inputs) -> typing.Any:
         groups = self.scatter(**inputs)
-        # TODO: build pipeline
-        template_pipeline, start_node_handle, config = self.get_inner_pipeline()
-        pipeline = Pipeline(template_pipeline.pipeline_name)
-        inputs_per_step = {}
-        output_steps_to_groups = {}
-        for key, value in groups.items():
-            node_mapping = {}
-            for node in template_pipeline.nodes:
-                if node.is_input and node.is_output:
-                    handle = pipeline.add_source(node.factory,
-                                                 is_sink=True,
-                                                 filename=node.output_filename + '__' + key,
-                                                 name=node.name + '-' + key if node.name is not None else None)
-                elif node.is_input:
-                    handle = pipeline.add_source(node.factory,
-                                                 name=node.name + '-' + key if node.name is not None else None)
-                elif node.is_output:
-                    handle = pipeline.add_sink(node.factory,
-                                               filename=node.output_filename + '__' + key,
-                                               name=node.name + '-' + key if node.name is not None else None)
-                else:
-                    handle = pipeline.add_step(node.factory,
-                                               name=node.name + '-' + key if node.name is not None else None)
-                node_mapping[node.handle] = handle
-            for connection in template_pipeline.connections:
-                pipeline.connect(node_mapping[connection.source],
-                                 node_mapping[connection.target],
-                                 connection.label)
-            # TODO: ScatterGather save format
-            # TODO: output steps; each output step
-            #       in the "template pipeline" should be
-            #       gathered individually.
-
+        pipeline, config, inputs_per_step, output_steps_by_step, output_steps_by_group = self._build_pipeline(groups)
         plan = pipeline.build(config, logger=self.logger)
-        results_per_group = await plan.execute_async(
+        pipeline_results = await plan.execute_async(
             output_directory=os.path.join(
                 self._output_directory, 'nested'
             ),
@@ -57,12 +27,74 @@ class ScatterGather(abc.ABC, PipelineStep):
                 self._checkpoint_directory, 'nested'
             ),
             precomputed_inputs_by_step=inputs_per_step,
-            __return_values=set(output_steps_to_groups.keys())
+            __return_values=set(
+                itertools.chain.from_iterable(output_steps_by_step.values())
+            )
         )
-        return self.gather(
-            **{output_steps_to_groups[k]: v
-               for k, v in results_per_group.items()}
-        )
+        return self._build_output(pipeline_results,
+                                  output_steps_by_group,
+                                  output_steps_by_step)
+
+    def _build_pipeline(self, groups: dict[str, typing.Any]):
+        template_pipeline, start_node_handle, config = self.get_inner_pipeline()
+        pipeline = Pipeline(template_pipeline.pipeline_name)
+        inputs_per_step = {}
+        output_steps_by_group = collections.defaultdict(set)
+        output_steps_by_step = collections.defaultdict(set)
+        for key, value in groups.items():
+            node_mapping = {}
+            for node in template_pipeline.nodes:
+                if node.is_input and node.is_output:
+                    if node.name is None:
+                        raise ValueError('Output node must have a set name.')
+                    handle = pipeline.add_source(node.factory,
+                                                 is_sink=True,
+                                                 filename=node.output_filename + '__' + key,
+                                                 name=node.name + '-' + key)
+                    output_steps_by_step[node.name].add(handle)
+                    output_steps_by_group[key].add(handle)
+                elif node.is_input:
+                    handle = pipeline.add_source(node.factory,
+                                                 name=(node.name + '-' + key) if node.name is not None else None)
+                elif node.is_output:
+                    if node.name is None:
+                        raise ValueError('Output node must have a set name.')
+                    handle = pipeline.add_sink(node.factory,
+                                               filename=node.output_filename + '__' + key,
+                                               name=node.name + '-' + key)
+                    output_steps_by_step[node.name].add(handle)
+                    output_steps_by_group[key].add(handle)
+                else:
+                    handle = pipeline.add_step(node.factory,
+                                               name=(node.name + '-' + key) if node.name is not None else None)
+                node_mapping[node.handle] = handle
+                if node.handle == start_node_handle:
+                    inputs_per_step[handle] = value
+            for connection in template_pipeline.connections:
+                pipeline.connect(node_mapping[connection.source],
+                                 node_mapping[connection.target],
+                                 connection.label)
+        return pipeline, config, inputs_per_step, output_steps_by_step, output_steps_by_group
+
+    def _build_output(self,
+                      pipeline_results: dict[PipelineStepHandle, typing.Any],
+                      output_steps_by_group: dict[str, set[PipelineStepHandle]],
+                      output_steps_by_step: dict[str, set[PipelineStepHandle]]) -> dict[str, typing.Any]:
+        group_by_handle = {}
+        for group, steps in output_steps_by_group.items():
+            for step in steps:
+                group_by_handle[step] = group
+        step_by_handle = {}
+        for name, steps in output_steps_by_step.items():
+            for step in steps:
+                step_by_handle[step] = name
+        outputs = collections.defaultdict(dict)
+        for handle, result in pipeline_results.items():
+            outputs[step_by_handle[handle]][group_by_handle[handle]] = result
+        return {
+            step: self.gather(key=step, **group)
+            for step, group in outputs.items()
+        }
 
     @abc.abstractmethod
     def scatter(self, **inputs) -> dict[str, typing.Any]:
@@ -73,7 +105,7 @@ class ScatterGather(abc.ABC, PipelineStep):
         pass
 
     @abc.abstractmethod
-    def gather(self, **results) -> typing.Any:
+    def gather(self, *, key: str, **results) -> typing.Any:
         pass
 
     def get_checkpoint_metadata(self) -> typing.Any:
@@ -89,39 +121,3 @@ class ScatterGather(abc.ABC, PipelineStep):
         # especially since steps in the inner pipeline
         # can still be cached.
         return False
-
-
-class ScatterGatherInput(PipelineStep):
-
-    @classmethod
-    def supports_step_as_input(cls, step: type[PipelineStep], label: str) -> bool:
-        return True
-
-    @staticmethod
-    def get_input_labels() -> list[str | type(...)]:
-        return [...]
-
-    async def execute(self, **inputs) -> typing.Any:
-        return inputs
-
-    @staticmethod
-    def save_result(path: str, result: typing.Any):
-        pass
-
-    @staticmethod
-    def load_result(path: str):
-        pass
-
-    def get_checkpoint_metadata(self) -> typing.Any:
-        return {}
-
-    def checkpoint_is_valid(self, metadata: typing.Any) -> bool:
-        return True
-
-    @classmethod
-    def get_arguments(cls) -> dict[str, arguments.Argument]:
-        return {}
-
-    @classmethod
-    def get_constraints(cls) -> list[constraints.Constraint]:
-        return []
