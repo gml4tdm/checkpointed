4d32a7499a35518968cd2639e13adb1afad917d6
Jesse Maarleveld <jessemaarleveld@gmail.com>
2024-01-16 14:49:25 +0100

    Update checkpointed public library

--- checkpointed/checkpointed/__init__.py
+++ checkpointed/checkpointed/__init__.py
@@ -1,4 +1,5 @@
 from checkpointed_core import Pipeline, PipelineStep, PipelineStepHandle, ExecutionPlan
+from checkpointed_core.parameters import Config, ConfigFactory
 from checkpointed_core.parameters import arguments, constraints
 import checkpointed_steps as steps
 
